9832242e064e7840422c9279663ca0bd0551347a
Irfan <shaikh.irfan.oct16@gmail.com>
2023-05-28 01:06:13 +0530

    add . algorithm

--- git.py
+++ git.py
@@ -1,7 +1,9 @@
+
+
 import sqlite3
 import os
 import sys
-from irfan import init
+from irfan import init, add
 
 
 def main():
@@ -18,14 +20,21 @@ def main():
     match command:
         case 'init':
             init()
-        # case 'add':
-        #     #  check the len of the sys.argv
-        #     if len(sys.argv) != 3:
-        #         sys.exit('Invalid command => Usage: git.py add <file>')
+        case 'add':
+            #  check the len of the sys.argv
+            if len(sys.argv) != 3:
+                sys.exit('Invalid command => Usage: git.py add <file>')
             
-        #     # check if atleast one file is given
-        #     add()
-        #     sys.exit('File added successfully')
+            #  check if database exists
+            if not os.path.exists('.zit/database.db'):
+                sys.exit('error: zit is not initialized')
+
+            # check if atleast one file is given
+            try:
+                add(os.getcwdb())
+            except:
+                sys.exit('error: something went wrong')
+            print('File added successfully')
         # case 'commit':
         #     # check len of the sys.argv
         #     # check m flag
--- irfan.py
+++ irfan.py
@@ -1,30 +1,65 @@
+# !C:\msys64\mingw64\bin\python.exe
 import os
 import sqlite3
+import pathlib
+# from yogesh import working_tree_insert, commitfolders_insert, folder_insert, files_insert
+
 
 def init():
     #create .git folder
     os.mkdir('.zit')
+    
+    # check if the database already exists
+    if os.path.exists('.zit/database.db'):
+        print('error: zit already initialized')
+        return
     
     # make database.db in that folder and create the tables
     # os.chdir('.zit')
     db = sqlite3.connect('.zit/database.db')
     cursor = db.cursor()
     
-    # - working tree (commit_id unique primary  key, message, branch_id, Branch_name)
-    createtableCommand1 = '''CREATE TABLE IF NOT EXISTS working_tree(commit_id INTEGER PRIMARY KEY, message TEXT, branch_id INTEGER, branch_name TEXT)'''
+    # - working tree (commit_id unique primary  key, message, Branch_name, time TEXT)
+    createtableCommand1 = '''CREATE TABLE working_tree(commit_id INTEGER PRIMARY KEY, message TEXT, branch_name TEXT DEFAULT 'master', time TEXT)'''
     # - commitfolders(commit_id, folder_id, file_id)
-    createtableCommand2 = '''CREATE TABLE IF NOT EXISTS commitfolders(commit_id INTEGER, folder_id INTEGER, file_id INTEGER)'''
-    # - folder (folder_id, folder_name, folder_id, file_id)
-    createtableCommand3 = '''CREATE TABLE IF NOT EXISTS folder(id INTEGER PRIMARY KEY, folder_id INTEGER, folder_name TEXT,ssub_file INTEGER, file_id INTEGER)'''
-    # - files (id, file_name, content)
-    createtableCommand4 = '''CREATE TABLE IF NOT EXISTS files(file_id INTEGER PRIMARY KEY, file_name TEXT, content TEXT)'''
+    createtableCommand2 = '''CREATE TABLE commitfolders(commit_id INTEGER, folder_file_id INTEGER)'''
+    # - folder (folder_id, folder_name, folder_file_id, content)
+    createtableCommand3 = '''CREATE TABLE folder(id INTEGER PRIMARY KEY, path TEXT, subfolder_file_id INTEGER, content TEXT)'''
     
     # excute the commands
     cursor.execute(createtableCommand1)
     cursor.execute(createtableCommand2)
     cursor.execute(createtableCommand3)
-    cursor.execute(createtableCommand4)
     
     # commiting changes
     db.commit()
     db.close()
+    print('success: zit initialized')
+    
+
+def add(root):
+    id_list = []
+    if len(os.listdir(root)) == 0:
+        #  add that folder to the database
+        #  get the last row id
+        #  append that id to the list
+        return id_list
+    for path in os.listdir(root):
+        if os.path.isfile(os.path.join(root, path)):
+            # add to file
+            # get the last row id
+            # append that id to the list
+            ''''''
+        else:
+            ids = add(os.path.join(root, path))
+            # add that id to with name of root, and subfolder as null and that id to file_id
+            # get the last row id
+            # append that id to the id_list
+    return id_list
+    
+
+
+if __name__ == '__main__':
+    # for path in add_helper(os.getcwdb(), []):
+    #     print(path)
+    add(os.getcwdb())
