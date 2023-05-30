84e4038d30bdd3a9ac53d4fb12167b5b9f72a05b
thelifeisstrange <yogeshpk0123@gmail.com>
2023-05-30 06:37:50 +0530

    Turtle

--- yogesh.py
+++ yogesh.py
@@ -4,4 +4,9 @@
 # - working tree (commit_id unique primary key, message, Branch_name, time Text)
 #  - commitfolders(commit_id, folder_id, file_id)
 #  - folder (folder_id, folder_name, subfolder_id, file_id)
-#  - files (id, file_name, content)
+#  - files (id, file_name, content){ import os, os me se path lo; .zit folder me jao jis me database hai. agar databse raha to insert karo nahi raha to error raise karo, 
+
+
+
+
+
