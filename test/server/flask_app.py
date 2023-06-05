71199b451206288dc81a6c18280ccd454eac2c92
Irfan <shaikh.irfan.oct16@gmail.com>
2023-06-06 02:28:05 +0530

    homepage

--- test/server/flask_app.py
+++ test/server/flask_app.py
@@ -5,10 +5,139 @@ import socket
 
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-    return render_template("index.html")
+# Sample data - Replace this with your actual data retrieval logic
+users = [
+    {
+        'username': 'user1',
+        'files': ['filename1', 'filename2', 'filename3']
+    },
+    {
+        'username': 'user2',
+        'files': ['filename1', 'filename2']
+    }
+]
 
+@app.route('/')
+def index():
+    html = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>ZITHUB</title>
+        <style>
+            body {
+                background-color: #f2f2f2;
+                font-family: Arial, sans-serif;
+                margin: 0;
+                padding: 20px;
+            }
+            
+            h1 {
+                color: #333;
+            }
+            
+            .card {
+                background-color: #fff;
+                border-radius: 5px;
+                box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+                padding: 20px;
+                margin-bottom: 20px;
+            }
+            
+            .card h3 {
+                margin-top: 0;
+            }
+            
+            ul {
+                margin: 0;
+                padding: 0;
+                list-style-type: none;
+            }
+            
+            li {
+                margin-bottom: 5px;
+            }
+            
+            .copy-button {
+                background-color: transparent;
+                border: none;
+                color: #4CAF50;
+                padding: 0;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+                font-size: 14px;
+                margin-top: 10px;
+                cursor: pointer;
+            }
+            
+            .tick-mark {
+                color: #4CAF50;
+                display: none;
+            }
+        </style>
+        <script>
+            function copyToClipboard(text, buttonId) {
+                const el = document.createElement('textarea');
+                el.value = text;
+                document.body.appendChild(el);
+                el.select();
+                document.execCommand('copy');
+                document.body.removeChild(el);
+                
+                const button = document.getElementById(buttonId);
+                button.innerHTML = '<i class="fas fa-check tick-mark"></i>';
+                button.disabled = true;
+                
+                setTimeout(function() {
+                    button.innerHTML = '<i class="fas fa-copy"></i>';
+                    button.disabled = false;
+                }, 2000);
+                
+            }
+        </script>
+        <script src="https://kit.fontawesome.com/a6b77cd6ac.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <h1>ZITHUB</h1>
+    """
+    
+    for user in os.listdir(os.path.join(os.getcwd(), "uploads")):
+        html += f"""
+        <div class="card">
+            <h3>{user}</h3>
+            <ul>
+        """
+        for file in os.listdir(os.path.join(os.getcwd(), "uploads", user)):
+            html += f"""
+                <li>
+                    <button class="copy-button" onclick="copyToClipboard('{file}')">Zit Clone</button>
+                    {file}
+                </li>
+            """
+        html += """
+            </ul>
+        </div>
+        """
+    
+    html += """
+    </body>
+    </html>
+    """
+    
+    return html
+
+
+@app.route('/generalupload', methods=['GET'])
+def general():
+    sendingTestpath = os.path.join(os.getcwd(), "sended")
+    # save the file send by the user
+    f = request.files['file']
+    f.save(os.path.join(sendingTestpath, f.filename))
+    return render_template("ack.html", name=f.filename)
+
+
+    
 @app.route('/upload', methods=['POST'])
 def upload():
     if request.method == 'POST':
