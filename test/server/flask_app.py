c0028241f4fdda224748d5b68c76c36e044cd6f0
Basit-Perampalli <apeerampalli@gmail.com>
2023-06-06 16:43:57 +0530

    changes in flask app server side

--- test/server/flask_app.py
+++ test/server/flask_app.py
@@ -2,124 +2,13 @@ import os
 from flask import Flask, request, render_template, send_file
 import re
 import socket
+import main
 
 app = Flask(__name__)
 @app.route('/')
 def index():
-    html = """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>ZITHUB</title>
-        <style>
-            body {
-                background-color: #f2f2f2;
-                font-family: Arial, sans-serif;
-                margin: 0;
-                padding: 20px;
-            }
-            
-            h1 {
-                color: #333;
-            }
-            
-            .card {
-                background-color: #fff;
-                border-radius: 5px;
-                box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-                padding: 20px;
-                margin-bottom: 20px;
-            }
-            
-            .card h3 {
-                margin-top: 0;
-            }
-            
-            ul {
-                margin: 0;
-                padding: 0;
-                list-style-type: none;
-            }
-            
-            li {
-                margin-bottom: 5px;
-            }
-            
-            .copy-button {
-                background-color: transparent;
-                border: none;
-                color: #4CAF50;
-                padding: 0;
-                text-align: center;
-                text-decoration: none;
-                display: inline-block;
-                font-size: 14px;
-                margin-top: 10px;
-                cursor: pointer;
-            }
-            
-            .tick-mark {
-                color: #4CAF50;
-                display: none;
-            }
-        </style>
-        <script>
-            function copyToClipboard(text, buttonId) {
-                const el = document.createElement('textarea');
-                el.value = text;
-                document.body.appendChild(el);
-                el.select();
-                document.execCommand('copy');
-                document.body.removeChild(el);
-                
-                const button = document.getElementById(buttonId);
-                button.innerHTML = '<i class="fas fa-check tick-mark"></i>';
-                button.disabled = true;
-                
-                setTimeout(function() {
-                    button.innerHTML = '<i class="fas fa-copy"></i>';
-                    button.disabled = false;
-                }, 2000);
-                
-            }
-        </script>
-        <script src="https://kit.fontawesome.com/a6b77cd6ac.js" crossorigin="anonymous"></script>
-    </head>
-    <body>
-        <h1>ZITHUB</h1>
-    """
-
-    if not os.path.exists("uploads"):
-        html += """
-            </body>
-            </html>
-            """
-        return html 
-
-    for user in os.listdir(os.path.join(os.getcwd(), "uploads")):
-        html += f"""
-        <div class="card">
-            <h3>{user}</h3>
-            <ul>
-        """
-        for file in os.listdir(os.path.join(os.getcwd(), "uploads", user)):
-            html += f"""
-                <li>
-                    <button class="copy-button" onclick="copyToClipboard('{file}')">Zit Clone</button>
-                    {file}
-                </li>
-            """
-        html += """
-            </ul>
-        </div>
-        """
+    return main.index()
     
-    html += """
-    </body>
-    </html>
-    """
-    
-    return html
 
 
 @app.route('/generalupload', methods=['GET'])
---
+++ test/server/main.py
@@ -0,0 +1,120 @@
+import os
+import socket
+
+def index():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    html = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <style>
+            body {
+                background-color: #f2f2f2;
+                font-family: Arial, sans-serif;
+                margin: 0;
+                padding: 20px;
+            }
+
+            h1 {
+                color: #333;
+            }
+
+            .card {
+                background-color: #fff;
+                border-radius: 5px;
+                box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+                padding: 20px;
+                margin-bottom: 20px;
+            }
+
+            .card h3 {
+                margin-top: 0;
+            }
+
+            ul {
+                margin: 0;
+                padding: 0;
+                list-style-type: none;
+            }
+
+            li {
+                margin-bottom: 5px;
+            }
+
+            .copy-button {
+                background-color: transparent;
+                border: none;
+                color: #4CAF50;
+                padding: 0;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+                font-size: 14px;
+                margin-top: 10px;
+                cursor: pointer;
+            }
+
+            .tick-mark {
+                color: #4CAF50;
+                display: none;
+            }
+        </style>
+
+        <title>ZITHUB</title>
+        <script>
+            function copyToClipboard(file, user) {
+                var textToCopy = f"{ip_address}"+','+file + ', ' + user;
+                
+                var tempInput = document.createElement("textarea");
+                tempInput.value = textToCopy;
+                document.body.appendChild(tempInput);
+                
+                tempInput.select();
+                tempInput.setSelectionRange(0, 99999);
+                
+                document.execCommand("copy");
+                
+                document.body.removeChild(tempInput);
+                
+            }
+        </script>
+    </head>
+    <body>
+        <h1>ZITHUB</h1>
+    """
+
+    if not os.path.exists("uploads"):
+        html += """
+            </body>
+            </html>
+            """
+        return html 
+
+    for user in os.listdir(os.path.join(os.getcwd(), "uploads")):
+        
+        html += f"""
+        <div class="card">
+            <h3>{user}</h3>
+            <ul>
+        """
+        for file in os.listdir(os.path.join(os.getcwd(), "uploads", user)):
+            if not file.endswith(".db"):
+                continue
+            html += f"""
+                <li>
+                    <button class="copy-button" onclick="copyToClipboard('{file}', '{user}')">Zit Clone</button>
+                    {file}
+                </li>
+            """
+        html += """
+            </ul>
+        </div>
+        """
+    
+    html += """
+    </body>
+    </html>
+    """
+    
+    return html
